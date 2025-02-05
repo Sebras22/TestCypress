09a90faa2f4cd7c539fb87c926769564f8626c5f
Sebras22 <sebastien.branly@efrei.net>
2025-02-05 10:45:35 +0100

    Maj

--- K6/Script.js
+++ K6/Script.js
@@ -1,5 +1,6 @@
 import { sleep, group } from "k6";
 import http from "k6/http";
+import browser from "k6/browser";
 
 export const options = {
     cloud: {
@@ -11,75 +12,44 @@ export const options = {
     thresholds: {},
     scenarios: {
         Scenario_1: {
-            executor: "ramping-vus",
-            gracefulStop: "30s",
-            stages: [
-                { target: 20, duration: "1m" },
-                { target: 20, duration: "3m30s" },
-                { target: 0, duration: "1m" },
-            ],
-            gracefulRampDown: "30s",
-            exec: "scenario_1",
-        },
-        Imported_HAR: {
             executor: "per-vu-iterations",
             gracefulStop: "30s",
             vus: 80,
             iterations: 400,
             maxDuration: "1h",
-            exec: "imported_HAR",
+            exec: "scenario_1",
         },
     },
 };
 
-// Scenario: Scenario_1 (executor: ramping-vus)
-
 export function scenario_1() {
     let response;
 
-    group(
-        "page_1 - https://crm.akov-formation.fr/datas/upload/fj/cv/r8/vsdkBRCemP.html",
-        function () {
-            response = http.get(
-                "https://crm.akov-formation.fr/datas/upload/fj/cv/r8/vsdkBRCemP.html",
-                {
-                    headers: {
-                        dnt: "1",
-                        "upgrade-insecure-requests": "1",
-                        "sec-ch-ua":
-                            '"Not A(Brand";v="8", "Chromium";v="132", "Google Chrome";v="132"',
-                        "sec-ch-ua-mobile": "?0",
-                        "sec-ch-ua-platform": '"Windows"',
-                    },
-                }
-            );
-        }
-    );
+    group("Load page", function () {
+        response = http.get(
+            "https://crm.akov-formation.fr/datas/upload/fj/cv/r8/vsdkBRCemP.html",
+            {
+                headers: {
+                    dnt: "1",
+                    "upgrade-insecure-requests": "1",
+                    "sec-ch-ua":
+                        '"Not A(Brand";v="8", "Chromium";v="132", "Google Chrome";v="132"',
+                    "sec-ch-ua-mobile": "?0",
+                    "sec-ch-ua-platform": '"Windows"',
+                },
+            }
+        );
+    });
     sleep(0.2);
-}
-
-// Scenario: Imported_HAR (executor: per-vu-iterations)
-
-export function imported_HAR() {
-    let response;
-
-    group(
-        "page_1 - https://crm.akov-formation.fr/datas/upload/fj/cv/r8/vsdkBRCemP.html",
-        function () {
-            response = http.get(
-                "https://crm.akov-formation.fr/datas/upload/fj/cv/r8/vsdkBRCemP.html",
-                {
-                    headers: {
-                        dnt: "1",
-                        "upgrade-insecure-requests": "1",
-                        "sec-ch-ua":
-                            '"Not A(Brand";v="8", "Chromium";v="132", "Google Chrome";v="132"',
-                        "sec-ch-ua-mobile": "?0",
-                        "sec-ch-ua-platform": '"Windows"',
-                    },
-                }
-            );
-        }
-    );
-    sleep(0.2);
+    group("Interact with task manager", function () {
+        browser.visit(
+            "https://crm.akov-formation.fr/datas/upload/fj/cv/r8/vsdkBRCemP.html",
+            function () {
+                browser.fill("#taskInput", "New Task");
+                browser.click("button:contains('Ajouter')");
+                sleep(1);
+                browser.click("button:contains('Supprimer')");
+            }
+        );
+    });
 }
